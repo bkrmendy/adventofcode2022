96fcf19379706b2771bbb9e019a051da2840a7b3
bkrmendy <korber314@gmail.com>
2022-12-01 11:16:41 +0100

    day 1

--- challenges/Day01.hs
+++ challenges/Day01.hs
@@ -1,18 +1,21 @@
 module Main where
 
 import Advent (challenge)
-import Utils ()
+import Utils (readInt)
+import Data.List (sort)
+import Data.List.Split (splitOn)
 
-type Challenge = String
+type Challenge = [[Int]]
 
 parse :: String -> Challenge
-parse = id
+parse = map parseElf . splitOn "\n\n"
+  where parseElf = map readInt . lines
 
-part1 :: Challenge -> String
-part1 = id
+part1 :: Challenge -> Int
+part1 = maximum . map sum
 
-part2 :: Challenge -> String
-part2 = id
+part2 :: Challenge -> Int
+part2 = sum . take 3 . reverse . sort . map sum
 
 main :: IO ()
 main = challenge 01 parse part1 part2
