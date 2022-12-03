096bc9902c9b2d92994785cd93c52310632eabe5
bkrmendy <korber314@gmail.com>
2022-12-03 10:48:56 +0100

    day 3 - chunksOf

--- challenges/Day03.hs
+++ challenges/Day03.hs
@@ -3,7 +3,7 @@ module Main where
 import Advent (challenge)
 import qualified Data.Set as S
 import Data.Char (ord, isLower, isUpper)
-import Data.Bifunctor (bimap)
+import Data.List.Split (chunksOf)
 import Data.List (foldl')
 
 type Compartment = S.Set Char
@@ -13,29 +13,24 @@ type Challenge = [String]
 halve :: [a] -> ([a], [a]) 
 halve xs = splitAt s xs
     where s = length xs `div` 2
-    
-threes :: [a] -> [[a]]
-threes [] = []
-threes (a:b:c:rest) = [a, b, c] : threes rest
-threes end = [end]
-    
-threentersection :: (Ord a) => [S.Set a] -> S.Set a
-threentersection [] = S.empty
-threentersection (first:rest) = foldl' S.intersection first rest
+        
+multisection :: (Ord a) => [S.Set a] -> S.Set a
+multisection [] = S.empty
+multisection (first:rest) = foldl' S.intersection first rest
 
 priority :: Char -> Int
 priority c | isLower c = ord c - ord 'a' + 1
 priority c | isUpper c = ord c - ord 'A' + 27
 priority _ = 0
 
-compartments :: String -> (Compartment, Compartment)
-compartments l = bimap S.fromList S.fromList $ halve l
+compartments :: String -> [Compartment]
+compartments l = (\(a, b) -> [S.fromList a, S.fromList b]) $ halve l
   
 part1 :: Challenge -> Int
-part1 = sum . concatMap (map priority . S.elems . uncurry S.intersection . compartments)
+part1 = sum . concatMap (map priority . S.elems . multisection . compartments)
 
 part2 :: Challenge -> Int
-part2 = sum . concatMap (map priority . S.elems . threentersection . map S.fromList) . threes
+part2 = sum . concatMap (map priority . S.elems . multisection . map S.fromList) . chunksOf 3
 
 main :: IO ()
 main = challenge 03 lines part1 part2
