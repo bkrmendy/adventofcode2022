b6d50af1c3052ab715510ddac0901a10c64f2a8a
bkrmendy <korber314@gmail.com>
2022-12-05 08:32:53 +0100

    day 5

---
+++ challenges/Day05.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import Advent (challenge)
+import Utils (readInt)
+
+import Data.List.Split (splitOn)
+import Data.List (foldl', transpose)
+import Data.Function ((&))
+import Data.Char (isUpper)
+import Data.Bifunctor (first)
+
+type Challenge = ([[Char]], [(Int, Int, Int)])
+
+move :: String -> (Int, Int, Int)
+move i = (readInt n, readInt from, readInt to)
+  where [_, n, _, from, _, to] = words i 
+
+sstacks :: String -> [[Char]]
+sstacks = filter (not . null) . map (filter isUpper) . transpose . lines
+
+parse :: String -> Challenge
+parse i = (sstacks starting, map move $ lines script)
+  where [starting, script] = splitOn "\n\n" i
+  
+popAt :: Int -> Int -> [[Char]] -> ([Char], [[Char]])
+popAt 1 p (h:rest)= let (pp, hh) = splitAt p h in (pp, hh:rest)
+popAt n p (h:rest) = let (c, hh) = popAt (n - 1) p rest in (c, h:hh)
+
+pushAt :: Int -> [Char] -> [[Char]] -> [[Char]]
+pushAt 1 c (h:rest) = (c ++ h):rest
+pushAt n c (h:rest) = h : pushAt (n - 1) c rest
+
+type Crane = [Char] -> [Char]
+
+step :: Crane -> (Int, Int, Int) -> [[Char]] -> [[Char]]
+step crane (n, from, to) s = popAt from n s
+                           & first crane
+                           & uncurry (pushAt to)
+
+result :: [[Char]] -> String
+result = map head
+
+run :: Crane -> Challenge -> String
+run crane = result . uncurry (foldl' (flip (step crane)))  
+
+main :: IO ()
+main = challenge 05 parse (run reverse) (run id)
