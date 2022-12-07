063952a417cde2ccd6b5b0a6375214765fea9350
bkrmendy <korber314@gmail.com>
2022-12-07 09:00:33 +0100

    day 7 - cleanup

--- challenges/Day07.hs
+++ challenges/Day07.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Advent (challenge)
 import Utils (readInt)
-import Debug.Trace (traceShow)
 
 import Data.List (foldl', sort)
 import qualified Data.Map.Strict as M
@@ -31,7 +30,7 @@ command path tree ["$", "cd", dir] = (dir:path, tree)
 command path tree ["$", "ls"] = (path, tree)
 command path tree ["dir", name] = (path, put (reverse path) tree (name, Dir M.empty))
 command path tree [size, filename] = (path, put (reverse path) tree (filename, File (readInt size)))
-command _ _ c = error $ "Unrecongized command: " <> show c
+command _ _ c = error $ "Unrecognized command: " <> show c
 
 dirSize :: Subtree -> Int
 dirSize = foldl' go 0 . M.elems
@@ -39,22 +38,23 @@ dirSize = foldl' go 0 . M.elems
         go acc (Dir contents) = acc + dirSize contents  
 
 sizes :: Subtree -> [Int]
-sizes dir = dirSize dir : concatMap size (M.elems dir) 
-  where size :: INode -> [Int]
-        size (File _) = []
-        size (Dir contents) = sizes contents
+sizes dir = dirSize dir : concatMap go (M.elems dir) 
+  where go (File _) = []
+        go (Dir contents) = sizes contents
 
 root :: Subtree
 root = M.fromList [("/", Dir M.empty)]
 
+buildFs :: [[String]] -> Subtree
+buildFs = snd . foldl' (uncurry command) ([], root)
+
 part1 :: Challenge -> Int
-part1 = sum . filter (<= 100000) . sizes . snd . foldl' (uncurry command) ([], root)
+part1 = sum . filter (<= 100000) . sizes . buildFs
 
 part2 :: Challenge -> Int
-part2 commands = head $ dropWhile (\a -> (unused + a) < 30000000) $ sort $ sizes tree
-  where (_, tree) = foldl' (uncurry command) ([], root) commands
+part2 commands = head $ dropWhile (\a -> (total - a) > 40000000) $ sort $ sizes tree
+  where tree = buildFs commands
         total = dirSize tree
-        unused = 70000000 - total
 
 main :: IO ()
 main = challenge 07 parse part1 part2
